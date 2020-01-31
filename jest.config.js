d02862b1b943584df058612690a22a797c68e035
Gus Narea <gnarea@users.noreply.github.com>
2020-01-31 15:34:11 +0000

    Rename src/app to src/services

--- jest.config.js
+++ jest.config.js
@@ -22,7 +22,7 @@ module.exports = {
   collectCoverage: false,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  collectCoverageFrom: ["src/app/**/*.ts"],
+  collectCoverageFrom: ["src/services/**/*.ts"],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: "coverage",

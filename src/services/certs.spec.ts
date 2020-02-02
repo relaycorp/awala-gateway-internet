b849ea2f8e6f62fd32745c5837d0fd3512a019fc
Gus Narea <gnarea@users.noreply.github.com>
2020-02-02 20:44:01 +0000

    Fix static checks

--- src/services/certs.spec.ts
+++ src/services/certs.spec.ts
@@ -1,4 +1,4 @@
-/* tslint:disable:no-let */
+/* tslint:disable:no-let no-object-mutation */
 
 import { generateRSAKeyPair } from '@relaycorp/relaynet-core';
 import * as typegoose from '@typegoose/typegoose';

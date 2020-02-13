8637ac4c44309019e5c6943dcc1d20106b6283ed
Gus Narea <gnarea@users.noreply.github.com>
2020-02-13 23:04:10 +0000

    Refactor

--- src/services/cogrpc/server.ts
+++ src/services/cogrpc/server.ts
@@ -3,13 +3,14 @@ import { Server, ServerCredentials } from 'grpc';
 
 import * as service from './service';
 
+const NETLOC = '0.0.0.0:8080';
+
 export function runServer(): void {
   const server = new Server();
   server.addService(CargoRelayService, service);
-  const netloc = '0.0.0.0:8080';
-  const bindResult = server.bind(netloc, ServerCredentials.createInsecure());
+  const bindResult = server.bind(NETLOC, ServerCredentials.createInsecure());
   if (bindResult < 0) {
-    throw new Error(`Failed to listen on ${netloc}`);
+    throw new Error(`Failed to listen on ${NETLOC}`);
   }
   server.start();
 }

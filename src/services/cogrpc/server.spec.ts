3edf60594563b7154e3e6febd63fb0f3c63f733d
Gus Narea <gnarea@users.noreply.github.com>
2020-02-17 15:51:02 +0000

    Complete service implementation integration

--- src/services/cogrpc/server.spec.ts
+++ src/services/cogrpc/server.spec.ts
@@ -1,26 +1,44 @@
 /* tslint:disable:no-let */
 
 import { CargoRelayService } from '@relaycorp/relaynet-cogrpc';
+import { EnvVarError } from 'env-var';
 import * as grpc from 'grpc';
 
+import { configureMockEnvVars, mockSpy } from '../_test_utils';
 import { runServer } from './server';
 import * as cogrpcService from './service';
 
 const addServiceSpy = jest.spyOn(grpc.Server.prototype, 'addService');
 const bindSpy = jest.spyOn(grpc.Server.prototype, 'bind');
 const startSpy = jest.spyOn(grpc.Server.prototype, 'start');
+const makeServiceImplementationSpy = mockSpy(
+  jest.spyOn(cogrpcService, 'makeServiceImplementation'),
+);
 beforeEach(() => {
   addServiceSpy.mockReset();
   bindSpy.mockReset();
   startSpy.mockReset();
 });
 
+const stubMongoUri = 'mongo://example.com';
+const mockEnvVars = configureMockEnvVars({ MONGO_URI: stubMongoUri });
+
 describe('runServer', () => {
+  test('Environment variable MONGO_URI should be present', () => {
+    mockEnvVars({});
+
+    expect(runServer).toThrowWithMessage(EnvVarError, /MONGO_URI/);
+  });
+
   test('CogRPC service should be added', () => {
     runServer();
 
+    expect(makeServiceImplementationSpy).toBeCalledTimes(1);
+    expect(makeServiceImplementationSpy).toBeCalledWith({ mongoUri: stubMongoUri });
+    const serviceImplementation = makeServiceImplementationSpy.mock.results[0].value;
+
     expect(addServiceSpy).toBeCalledTimes(1);
-    expect(addServiceSpy).toBeCalledWith(CargoRelayService, cogrpcService);
+    expect(addServiceSpy).toBeCalledWith(CargoRelayService, serviceImplementation);
   });
 
   test('Server should listen on 0.0.0.0:8080', () => {
--- src/services/cogrpc/server.ts
+++ src/services/cogrpc/server.ts
@@ -1,13 +1,19 @@
 import { CargoRelayService } from '@relaycorp/relaynet-cogrpc';
+import { get as getEnvVar } from 'env-var';
 import { Server, ServerCredentials } from 'grpc';
 
-import * as service from './service';
+import { makeServiceImplementation } from './service';
 
 const NETLOC = '0.0.0.0:8080';
 
 export function runServer(): void {
+  const mongoUri = getEnvVar('MONGO_URI')
+    .required()
+    .asString();
+
   const server = new Server();
-  server.addService(CargoRelayService, service);
+  const serviceImplementation = makeServiceImplementation({ mongoUri });
+  server.addService(CargoRelayService, serviceImplementation);
   const bindResult = server.bind(NETLOC, ServerCredentials.createInsecure());
   if (bindResult < 0) {
     throw new Error(`Failed to listen on ${NETLOC}`);

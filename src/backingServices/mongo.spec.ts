2009d3f3ae93e31e530cf563f48798a99df1945e
Gus Narea <gnarea@users.noreply.github.com>
2020-05-26 23:13:42 +0100

    Fix formatting

--- src/backingServices/mongo.spec.ts
+++ src/backingServices/mongo.spec.ts
@@ -25,16 +25,13 @@ describe('createMongooseConnectionFromEnv', () => {
   test('Connection should use MONGO_URI', async () => {
     await createMongooseConnectionFromEnv();
 
-    expect(MOCK_MONGOOSE_CREATE_CONNECTION).toBeCalledWith(
-      MONGO_URI,
-      expect.anything(),
-    );
+    expect(MOCK_MONGOOSE_CREATE_CONNECTION).toBeCalledWith(MONGO_URI, expect.anything());
   });
 
   test('Mongoose connection should be returned', async () => {
     const connection = await createMongooseConnectionFromEnv();
 
-    expect(connection).toBe(MOCK_MONGOOSE_CONNECTION)
+    expect(connection).toBe(MOCK_MONGOOSE_CONNECTION);
   });
 
   test('Connection should be created with new URL parser', async () => {

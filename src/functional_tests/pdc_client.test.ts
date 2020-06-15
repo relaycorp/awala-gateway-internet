2e55d7eddd3d411baf544cde83d2926c9b63efbd
Gus Narea <gnarea@users.noreply.github.com>
2020-06-15 09:23:12 +0100

    Client test suite: implement nominal case

--- src/functional_tests/pdc_client.test.ts
+++ src/functional_tests/pdc_client.test.ts
@@ -1,3 +1,101 @@
-describe('PDC client', () => {
-  test.todo('Test');
+// tslint:disable:no-let
+
+import {
+  Certificate,
+  generateRSAKeyPair,
+  issueEndpointCertificate,
+  Parcel,
+} from '@relaycorp/relaynet-core';
+import { Stan } from 'node-nats-streaming';
+import { promisify } from 'util';
+
+import { bootstrapServiceData, setUpServices, tearDownServices } from './services';
+import {
+  connectToNatsStreaming,
+  OBJECT_STORAGE_BUCKET,
+  OBJECT_STORAGE_CLIENT,
+  sleep,
+} from './utils';
+
+const PONG_ENDPOINT_ADDRESS = 'http://pong:8080';
+
+beforeAll(async () => {
+  jest.setTimeout(15_000);
+  await tearDownServices();
+  await setUpServices(['pdc-outgoing-queue-worker']);
+  await sleep(2);
+  await bootstrapServiceData(false);
 });
+afterAll(tearDownServices);
+
+let senderPrivateKey: CryptoKey;
+let senderCertificate: Certificate;
+beforeAll(async () => {
+  const keyPair = await generateRSAKeyPair();
+  senderPrivateKey = keyPair.privateKey;
+
+  const tomorrow = new Date();
+  tomorrow.setDate(tomorrow.getDate() + 1);
+  senderCertificate = await issueEndpointCertificate({
+    issuerPrivateKey: senderPrivateKey,
+    subjectPublicKey: keyPair.publicKey,
+    validityEndDate: tomorrow,
+  });
+});
+
+let natsStreamingConnection: Stan;
+beforeAll(async () => (natsStreamingConnection = await connectToNatsStreaming()));
+afterAll(async () => natsStreamingConnection.close());
+
+describe('PDC client', () => {
+  test('Successfully delivered parcels should be taken off the queue', async () => {
+    const parcel = new Parcel(PONG_ENDPOINT_ADDRESS, senderCertificate, Buffer.from([]));
+
+    await queueParcel(parcel);
+
+    await sleep(2);
+    await expect(isParcelInStore(parcel)).resolves.toBeFalse();
+  });
+
+  test.todo('Parcels refused as invalid should be taken off the queue');
+
+  test.todo('Unsuccessfully delivered parcels should remain in the queue');
+});
+
+async function queueParcel(parcel: Parcel): Promise<void> {
+  // TODO: Use PoWebSockets once it's available, instead of messing with backing services
+
+  const objectKey = makeParcelObjectKey(parcel.id);
+  await OBJECT_STORAGE_CLIENT.putObject({
+    Body: Buffer.from(await parcel.serialize(senderPrivateKey)),
+    Bucket: OBJECT_STORAGE_BUCKET,
+    Key: objectKey,
+    Metadata: { 'parcel-expiry': Math.floor(parcel.expiryDate.getTime() / 1_000).toString() },
+  }).promise();
+
+  const natsPublish = promisify(natsStreamingConnection.publish).bind(natsStreamingConnection);
+  await natsPublish(
+    'crc-parcels',
+    JSON.stringify({
+      parcelExpiryDate: parcel.expiryDate,
+      parcelObjectKey: parcel.id,
+      parcelRecipientAddress: parcel.recipientAddress,
+    }),
+  );
+}
+
+async function isParcelInStore(parcel: Parcel): Promise<boolean> {
+  try {
+    await OBJECT_STORAGE_CLIENT.getObject({
+      Bucket: OBJECT_STORAGE_BUCKET,
+      Key: makeParcelObjectKey(parcel.id),
+    }).promise();
+  } catch (_) {
+    return false;
+  }
+  return true;
+}
+
+function makeParcelObjectKey(parcelId: string): string {
+  return `parcels/endpoint-bound/${parcelId}`;
+}
--- src/functional_tests/services.ts
+++ src/functional_tests/services.ts
@@ -20,22 +20,24 @@ const COMPOSE_OPTIONS = [
   'src/functional_tests/docker-compose.override.yml',
 ];
 
-export async function bootstrapServiceData(): Promise<void> {
-  await dockerCompose.exec('vault', ['vault', 'secrets', 'enable', '-path=gw-keys', 'kv-v2'], {
-    commandOptions: ['--env', `VAULT_ADDR=${VAULT_URL}`, '--env', `VAULT_TOKEN=${VAULT_TOKEN}`],
-    composeOptions: COMPOSE_OPTIONS,
-    log: true,
-  });
+export async function bootstrapServiceData(includeVault = true): Promise<void> {
+  if (includeVault) {
+    await dockerCompose.exec('vault', ['vault', 'secrets', 'enable', '-path=gw-keys', 'kv-v2'], {
+      commandOptions: ['--env', `VAULT_ADDR=${VAULT_URL}`, '--env', `VAULT_TOKEN=${VAULT_TOKEN}`],
+      composeOptions: COMPOSE_OPTIONS,
+      log: true,
+    });
+
+    await dockerCompose.run('cogrpc', ['src/bin/generate-keypairs.ts'], {
+      commandOptions: ['--rm'],
+      composeOptions: COMPOSE_OPTIONS,
+      log: true,
+    });
+  }
 
   await OBJECT_STORAGE_CLIENT.createBucket({
     Bucket: OBJECT_STORAGE_BUCKET,
   }).promise();
-
-  await dockerCompose.run('cogrpc', ['src/bin/generate-keypairs.ts'], {
-    commandOptions: ['--rm'],
-    composeOptions: COMPOSE_OPTIONS,
-    log: true,
-  });
 }
 
 // tslint:disable-next-line:readonly-array

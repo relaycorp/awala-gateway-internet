e7a280391331524639cb285627472507c1656637
Gus Narea <gnarea@users.noreply.github.com>
2020-09-21 17:27:50 +0100

    Update URL to CogRPC server in functional test suite

--- src/functional_tests/services.ts
+++ src/functional_tests/services.ts
@@ -7,7 +7,7 @@ import { get as getEnvVar } from 'env-var';
 import { IS_GITHUB, OBJECT_STORAGE_BUCKET, OBJECT_STORAGE_CLIENT, sleep } from './utils';
 
 export const GW_POHTTP_URL = 'http://127.0.0.1:8080';
-export const GW_GOGRPC_URL = 'http://127.0.0.1:8081/';
+export const GW_GOGRPC_URL = 'https://127.0.0.1:8081/';
 export const PONG_ENDPOINT_ADDRESS = 'http://pong:8080/';
 
 const VAULT_URL = getEnvVar('VAULT_URL').required().asString();

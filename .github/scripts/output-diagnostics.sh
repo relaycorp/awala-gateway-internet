4fe66dd8cb0b7bcdb60e9c4838abf9a793f4f699
Gus Narea <gnarea@users.noreply.github.com>
2020-11-18 18:07:29 +0000

    improve diagnostics

--- .github/scripts/output-diagnostics.sh
+++ .github/scripts/output-diagnostics.sh
@@ -3,10 +3,27 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
+# Functions
+
+print_header() {
+  local title="$1"
+
+  printf '#%.0s' {1..50}
+  echo "${title}"
+}
+
+# Main
+
 if ! command -v kubectl; then
   echo "Skipping because Kubernetes wasn't installed" >&2
   exit 1
 fi
+
+print_header "Pods"
+kubectl get pods
+
+print_header "Services"
+kubectl get services
 
 DEPLOYMENTS="$(
   kubectl get deploy \
@@ -15,8 +32,7 @@ DEPLOYMENTS="$(
 )"
 
 for deployment in ${DEPLOYMENTS}; do
-  printf '#%.0s' {1..50}
-  echo " Logs for ${deployment}"
+  print_header "Logs for ${deployment}"
 
   kubectl logs "deploy/${deployment}" --all-containers=true
 done

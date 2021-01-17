e67a771e7426bec59f37542f201fea9359e71b2b
Gus Narea <gnarea@users.noreply.github.com>
2021-01-17 19:49:53 +0000

    compute certificate model once

--- src/services/certs.ts
+++ src/services/certs.ts
@@ -1,17 +1,21 @@
 import { Certificate } from '@relaycorp/relaynet-core';
-import { getModelForClass } from '@typegoose/typegoose';
+import { getModelForClass, ReturnModelType } from '@typegoose/typegoose';
 import bufferToArray from 'buffer-to-arraybuffer';
 import { Connection } from 'mongoose';
 
 import { OwnCertificate } from './models';
 import { BasicLogger } from './types';
 
+let ownCertificateModel: ReturnModelType<typeof OwnCertificate, {}>;
+
 export async function retrieveOwnCertificates(
   connection: Connection,
   logger: BasicLogger | null = null,
 ): Promise<readonly Certificate[]> {
   logger?.info('Before getModelForClass()');
-  const ownCertificateModel = getModelForClass(OwnCertificate, { existingConnection: connection });
+  if (!ownCertificateModel) {
+    ownCertificateModel = getModelForClass(OwnCertificate, { existingConnection: connection });
+  }
   logger?.info('Before find()');
   const findOne = ownCertificateModel.find({});
   logger?.info('Before exec()');

b1fb71c8e46ff7c542c861d15d3559371bc4a0c2
Gus Narea <gnarea@users.noreply.github.com>
2021-01-18 11:57:55 +0000

    restore original use of getModelForClass()

--- src/services/certs.ts
+++ src/services/certs.ts
@@ -1,21 +1,17 @@
 import { Certificate } from '@relaycorp/relaynet-core';
-import { getModelForClass, ReturnModelType } from '@typegoose/typegoose';
+import { getModelForClass } from '@typegoose/typegoose';
 import bufferToArray from 'buffer-to-arraybuffer';
 import { Connection } from 'mongoose';
 
 import { OwnCertificate } from './models';
 import { BasicLogger } from './types';
 
-let ownCertificateModel: ReturnModelType<typeof OwnCertificate, {}>;
-
 export async function retrieveOwnCertificates(
   connection: Connection,
   logger: BasicLogger | null = null,
 ): Promise<readonly Certificate[]> {
   logger?.info('Before getModelForClass()');
-  if (!ownCertificateModel) {
-    ownCertificateModel = getModelForClass(OwnCertificate, { existingConnection: connection });
-  }
+  const ownCertificateModel = getModelForClass(OwnCertificate, { existingConnection: connection });
   logger?.info('Before find()');
   const findOne = ownCertificateModel.find({});
   logger?.info('Before exec()');

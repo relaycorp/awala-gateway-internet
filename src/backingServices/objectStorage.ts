00d9c9fb4a7f8798f945679ca6d446ca4f3773f1
Gus Narea <gnarea@users.noreply.github.com>
2021-01-25 18:54:25 +0000

    fix: Allow GCS bucket configuration (#387)

--- src/backingServices/objectStorage.ts
+++ src/backingServices/objectStorage.ts
@@ -3,9 +3,9 @@ import { get as getEnvVar } from 'env-var';
 
 export function initObjectStoreFromEnv(): ObjectStoreClient {
   const backend = getEnvVar('OBJECT_STORE_BACKEND').required().asString();
-  const endpoint = getEnvVar('OBJECT_STORE_ENDPOINT').required().asString();
-  const accessKeyId = getEnvVar('OBJECT_STORE_ACCESS_KEY_ID').required().asString();
-  const secretKey = getEnvVar('OBJECT_STORE_SECRET_KEY').required().asString();
+  const endpoint = getEnvVar('OBJECT_STORE_ENDPOINT').asString();
+  const accessKeyId = getEnvVar('OBJECT_STORE_ACCESS_KEY_ID').asString();
+  const secretKey = getEnvVar('OBJECT_STORE_SECRET_KEY').asString();
   const tlsEnabled = getEnvVar('OBJECT_STORE_TLS_ENABLED').default('true').asBool();
   return initObjectStoreClient(
     backend as AdapterType,

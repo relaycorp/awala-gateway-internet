43a90dfed18c5bea40521c4cad5c4402c0cf498d
Gus Narea <gnarea@users.noreply.github.com>
2021-01-25 18:43:25 +0000

    fix: Allow GCS bucket configuration

--- src/backingServices/objectStorage.spec.ts
+++ src/backingServices/objectStorage.spec.ts
@@ -7,25 +7,54 @@ import { initObjectStoreFromEnv } from './objectStorage';
 jest.mock('@relaycorp/object-storage');
 
 describe('initFromEnv', () => {
-  const requiredEnvVars = {
+  const envVars = {
     OBJECT_STORE_ACCESS_KEY_ID: 'the-access-key-id',
     OBJECT_STORE_BACKEND: 'minio',
     OBJECT_STORE_ENDPOINT: 'objects.example.com:9000',
     OBJECT_STORE_SECRET_KEY: 'the-secret-key',
   };
-  const mockEnvVars = configureMockEnvVars(requiredEnvVars);
+  const mockEnvVars = configureMockEnvVars(envVars);
 
-  test.each(Object.getOwnPropertyNames(requiredEnvVars))(
-    '%s should be required',
-    (envVarKey: string) => {
-      mockEnvVars({ ...requiredEnvVars, [envVarKey]: undefined });
+  test('OBJECT_STORE_BACKEND should be required', () => {
+    mockEnvVars({});
 
-      expect(() => initObjectStoreFromEnv()).toThrowWithMessage(EnvVarError, new RegExp(envVarKey));
-    },
-  );
+    expect(() => initObjectStoreFromEnv()).toThrowWithMessage(EnvVarError, /OBJECT_STORE_BACKEND/);
+  });
+
+  test('OBJECT_STORE_ENDPOINT should be optional', () => {
+    mockEnvVars({ ...envVars, OBJECT_STORE_ENDPOINT: undefined });
+
+    initObjectStoreFromEnv();
+
+    expect(initObjectStoreClient).toBeCalledWith(
+      expect.anything(),
+      undefined,
+      expect.anything(),
+      expect.anything(),
+      expect.anything(),
+    );
+  });
+
+  test('Credentials should be optional', () => {
+    mockEnvVars({
+      ...envVars,
+      OBJECT_STORE_ACCESS_KEY_ID: undefined,
+      OBJECT_STORE_SECRET_KEY: undefined,
+    });
+
+    initObjectStoreFromEnv();
+
+    expect(initObjectStoreClient).toBeCalledWith(
+      expect.anything(),
+      expect.anything(),
+      undefined,
+      undefined,
+      expect.anything(),
+    );
+  });
 
   test('OBJECT_STORE_TLS_ENABLED should be honored if present', () => {
-    mockEnvVars({ ...requiredEnvVars, OBJECT_STORE_TLS_ENABLED: 'false' });
+    mockEnvVars({ ...envVars, OBJECT_STORE_TLS_ENABLED: 'false' });
 
     initObjectStoreFromEnv();
 
@@ -39,7 +68,7 @@ describe('initFromEnv', () => {
   });
 
   test('TLS should be enabled if OBJECT_STORE_TLS_ENABLED is missing', () => {
-    mockEnvVars({ ...requiredEnvVars, OBJECT_STORE_TLS_ENABLED: undefined });
+    mockEnvVars({ ...envVars, OBJECT_STORE_TLS_ENABLED: undefined });
 
     initObjectStoreFromEnv();
 
@@ -53,15 +82,15 @@ describe('initFromEnv', () => {
   });
 
   test('Environment variables should be passed to constructor', () => {
-    mockEnvVars({ ...requiredEnvVars, OBJECT_STORE_TLS_ENABLED: undefined });
+    mockEnvVars({ ...envVars });
 
     initObjectStoreFromEnv();
 
     expect(initObjectStoreClient).toBeCalledWith(
-      requiredEnvVars.OBJECT_STORE_BACKEND,
-      requiredEnvVars.OBJECT_STORE_ENDPOINT,
-      requiredEnvVars.OBJECT_STORE_ACCESS_KEY_ID,
-      requiredEnvVars.OBJECT_STORE_SECRET_KEY,
+      envVars.OBJECT_STORE_BACKEND,
+      envVars.OBJECT_STORE_ENDPOINT,
+      envVars.OBJECT_STORE_ACCESS_KEY_ID,
+      envVars.OBJECT_STORE_SECRET_KEY,
       expect.anything(),
     );
   });

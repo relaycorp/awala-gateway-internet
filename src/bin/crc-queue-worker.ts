04b6baf3f80d10c433f2f1be5c5f4e1a0d78ce19
Gus Narea <gnarea@users.noreply.github.com>
2021-02-06 12:15:26 +0000

    integrate CRC queue

--- src/bin/crc-queue-worker.ts
+++ src/bin/crc-queue-worker.ts
@@ -4,6 +4,3 @@ import { processIncomingCrcCargo } from '../services/crcQueueWorker';
 
 const WORKER_ID = getEnvVar('WORKER_ID').required().asString();
 processIncomingCrcCargo(WORKER_ID);
-
-// tslint:disable-next-line:no-console
-console.log(`Starting queue worker ${WORKER_ID}`);

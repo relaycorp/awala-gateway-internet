211d3bcb65a497533d5fdb5bd3205f3b7ddbbe53
Gus Narea <gnarea@users.noreply.github.com>
2022-06-20 18:05:18 +0100

    Remove Certificate.scope

--- src/models.ts
+++ src/models.ts
@@ -25,11 +25,6 @@ export class CertificationPath {
 
   @prop({ required: true, expires: 0 })
   public expiryDate!: Date;
-
-  // TODO: Make field required once we can write migrations
-  // See https://github.com/relaycorp/relaynet-internet-gateway/issues/751
-  @prop()
-  public scope?: string;
 }
 
 export class PeerPublicKeyData {

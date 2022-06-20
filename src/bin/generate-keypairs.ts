f95b8a8a624836bcfc404294a7fde5476ada1bf9
Gus Narea <gnarea@users.noreply.github.com>
2022-06-20 15:57:13 +0100

    fix generate-keypairs.ts

--- src/bin/generate-keypairs.ts
+++ src/bin/generate-keypairs.ts
@@ -1,10 +1,5 @@
-import {
-  CertificateScope,
-  generateRSAKeyPair,
-  issueGatewayCertificate,
-} from '@relaycorp/relaynet-core';
+import { CertificationPath, issueGatewayCertificate } from '@relaycorp/relaynet-core';
 import { addDays } from 'date-fns';
-import { Connection } from 'mongoose';
 
 import { createMongooseConnectionFromEnv } from '../backingServices/mongo';
 import { initVaultKeyStore } from '../backingServices/vault';
@@ -24,35 +19,32 @@ async function main(): Promise<void> {
   try {
     const certificateStore = new MongoCertificateStore(connection);
 
-    await generateKeyPair(connection, certificateStore);
+    const config = new Config(connection);
+    const currentPrivateAddress = await config.get(ConfigKey.CURRENT_PRIVATE_ADDRESS);
+    if (currentPrivateAddress) {
+      LOGGER.info({ privateAddress: currentPrivateAddress }, `Gateway key pair already exists`);
+    } else {
+      await generateKeyPair(certificateStore, config);
+    }
   } finally {
     await connection.close();
   }
 }
 
 async function generateKeyPair(
-  connection: Connection,
   certificateStore: MongoCertificateStore,
+  config: Config,
 ): Promise<void> {
-  const config = new Config(connection);
-  const currentPrivateAddress = await config.get(ConfigKey.CURRENT_PRIVATE_ADDRESS);
-  if (currentPrivateAddress) {
-    LOGGER.info({ privateAddress: currentPrivateAddress }, `Gateway key pair already exists`);
-    return;
-  }
-  LOGGER.info(`Gateway key will be created because it doesn't already exist`);
-
-  const gatewayKeyPair = await generateRSAKeyPair();
-  const privateAddress = await privateKeyStore.saveIdentityKey(gatewayKeyPair.privateKey);
+  const { privateAddress, privateKey, publicKey } = await privateKeyStore.generateIdentityKeyPair();
 
   const gatewayCertificate = await issueGatewayCertificate({
-    issuerPrivateKey: gatewayKeyPair.privateKey,
-    subjectPublicKey: gatewayKeyPair.publicKey,
+    issuerPrivateKey: privateKey,
+    subjectPublicKey: publicKey,
     validityEndDate: addDays(new Date(), CERTIFICATE_TTL_DAYS),
   });
-  await certificateStore.save(gatewayCertificate, CertificateScope.PDA);
+  await certificateStore.save(new CertificationPath(gatewayCertificate, []), privateAddress);
 
-  await config.set(ConfigKey.CURRENT_PRIVATE_ADDRESS, privateAddress);
+  await config.set(ConfigKey.CURRENT_PRIVATE_ADDRESS, privateAddress!);
 
   LOGGER.info({ privateAddress }, 'Identity key pair was successfully generated');
 }

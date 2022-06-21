ce466050fb0daf217e1afa2d38647971f61f3947
Gus Narea <gnarea@users.noreply.github.com>
2022-06-21 17:17:10 +0100

    chore(release): Use `cm-push` from helm-push v0.10+

--- .github/scripts/push-helm-chart.sh
+++ .github/scripts/push-helm-chart.sh
@@ -5,4 +5,4 @@ set -o pipefail
 
 HELM_PACKAGE="$(find build/ -name 'relaynet-internet-gateway-*.tgz' | head -n1)"
 export HELM_EXPERIMENTAL_OCI=1
-helm push "${HELM_PACKAGE}" https://h.cfcr.io/relaycorp/public
+helm cm-push "${HELM_PACKAGE}" https://h.cfcr.io/relaycorp/public

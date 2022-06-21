a969b296310ad656d78f9838ab6558054d6da2f7
Gus Narea <gnarea@users.noreply.github.com>
2022-06-21 16:38:48 +0100

    chore(release): Set HELM_EXPERIMENTAL_OCI=1 when pushing chart

--- .github/scripts/push-helm-chart.sh
+++ .github/scripts/push-helm-chart.sh
@@ -4,4 +4,5 @@ set -o errexit
 set -o pipefail
 
 HELM_PACKAGE="$(find build/ -name 'relaynet-internet-gateway-*.tgz' | head -n1)"
+export HELM_EXPERIMENTAL_OCI=1
 helm push "${HELM_PACKAGE}" https://h.cfcr.io/relaycorp/public

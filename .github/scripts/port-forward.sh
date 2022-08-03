a6c89b6347167d1a25fb9fad97d2d33df7971d29
Gus Narea <gnarea@users.noreply.github.com>
2022-08-03 18:33:31 +0100

    ci: fix vault and pong

--- .github/scripts/port-forward.sh
+++ .github/scripts/port-forward.sh
@@ -9,7 +9,7 @@ set -x
 kubectl port-forward --address 127.0.0.1 svc/public-gateway-poweb 8080:8082 &
 kubectl port-forward --address 127.0.0.1 svc/public-gateway-pohttp 8081:8080 &
 kubectl port-forward --address 127.0.0.1 svc/public-gateway-cogrpc 8082:8081 &
-kubectl port-forward --address 127.0.0.1 svc/relaynet-pong-pohttp 8083:80 &
+kubectl port-forward --address 127.0.0.1 svc/pong-pohttp 8083:80 &
 kubectl port-forward --address 127.0.0.1 svc/nats 4222:4222 &
 kubectl port-forward --address 127.0.0.1 svc/minio 9000:9000 &
 
--- src/functionalTests/services.ts
+++ src/functionalTests/services.ts
@@ -3,5 +3,5 @@ export const GW_POHTTP_LOCAL_URL = 'http://127.0.0.1:8081';
 export const GW_COGRPC_LOCAL_URL = 'https://127.0.0.1:8082';
 export const GW_POWEB_LOCAL_PORT = 8080;
 
-export const PONG_INTERNET_ADDRESS = 'relaynet-pong-pohttp.default';
+export const PONG_INTERNET_ADDRESS = 'pong-pohttp.default';
 export const PONG_LOCAL_URL = 'http://127.0.0.1:8083';

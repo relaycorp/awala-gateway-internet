158c947828b8ef5420067dc425be4dc4d645c224
Gus Narea <gnarea@users.noreply.github.com>
2023-10-19 14:33:26 +0100

    fix nats port forward

--- src/functionalTests/utils/nats.ts
+++ src/functionalTests/utils/nats.ts
@@ -3,13 +3,16 @@ import { connect as stanConnect, Stan } from 'node-nats-streaming';
 import uuid from 'uuid-random';
 
 export function connectToNatsStreaming(): Promise<Stan> {
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
     const stanConnection = stanConnect(
       getEnvVar('NATS_CLUSTER_ID').required().asString(),
       `functional-tests-${uuid()}`,
       {
         url: getEnvVar('NATS_SERVER_URL').required().asString(),
       },
+    );
+    stanConnection.on('error', (err) =>
+      reject(new Error('Failed to connect to NATS', { cause: err })),
     );
     stanConnection.on('connect', resolve);
   });
